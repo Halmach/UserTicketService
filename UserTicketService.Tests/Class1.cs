386d32d6016949b55a59002799020925b4822caa
Khalmatov <large000@yandex.ru>
2022-01-11 21:54:00 +0300

    Добавьте файлы проекта.

---
+++ UserTicketService.Tests/Class1.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace UserTicketService.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(100 == 100);
+        }
+    }
+}
---
+++ UserTicketService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserTicketService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
