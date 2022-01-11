27d465ee1d5a3cb1fb625270702a1db8e7a9347c
Khalmatov <large000@yandex.ru>
2022-01-11 22:24:09 +0300

    Задание 16.2.4

---
+++ UserTicketService.Tests/CalculatorTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserTicketService.Tests
+{
+    [TestFixture]
+    class CalculatorTest
+    {
+        [Test]
+        public void SubtractionMustReturnCorrectValue()
+        {
+            var calc = new Calculator();
+            Assert.True(5 == calc.Subtraction(10, 5));
+        }
+    }
+}
---
+++ UserTicketService/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserTicketService
+{
+    public class Calculator
+    {
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
