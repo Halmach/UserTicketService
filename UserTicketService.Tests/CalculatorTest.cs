1997a03e694edef284fcbbed37025b73c5a4e069
Khalmatov <large000@yandex.ru>
2022-01-16 22:59:00 +0300

    Задание 16.4.3  AdditionalMustReturnCorrectValue MultiplicationMustReturnCorrectValue

--- UserTicketService.Tests/CalculatorTest.cs
+++ UserTicketService.Tests/CalculatorTest.cs
@@ -28,5 +28,21 @@ namespace UserTicketService.Tests
             var calc = new Calculator();
             Assert.Equal(5, calc.Division(10,2));
         }
+
+        [Fact]
+        public void AdditionalMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            int result = calculator.Addition(50, 10, 34);
+            Assert.Equal(94, result);
+        }
+
+        [Fact]
+        public void MultiplicationMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            int result = calculator.Multiplication(3, 2, 20);
+            Assert.Equal(120, result);
+        }
     }
 }
--- UserTicketService/Calculator.cs
+++ UserTicketService/Calculator.cs
@@ -15,5 +15,15 @@ namespace UserTicketService
         {
             return a / b;
         }
+
+        public int Addition(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        public int Multiplication(int a, int b, int c)
+        {
+            return a * b * c;
+        }
     }
 }
