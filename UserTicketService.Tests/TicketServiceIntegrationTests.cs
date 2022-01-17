94cf5cef2582120ec6b0181148eb00b2807b38ec
Khalmatov <large000@yandex.ru>
2022-01-17 23:05:22 +0300

    Интеграционные тесты vs. Unit-тесты

---
+++ UserTicketService.Tests/TicketServiceIntegrationTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserTicketService.Tests
+{
+    [TestFixture]
+    public class TicketServiceIntegrationTests
+    {
+        [Test]
+        public void SaveTicketMustAddTicketInBase()
+        {
+            var ticketServiceTest = new TicketService();
+            var newTicket = new Ticket(300, "Test ticket", 1000);
+
+            ticketServiceTest.SaveTicket(newTicket);
+
+            var allTicketsAfterAddingNewTicket = ticketServiceTest.GetAllTickets();
+            CollectionAssert.Contains(allTicketsAfterAddingNewTicket, newTicket);
+
+            ticketServiceTest.DeleteTicket(newTicket);
+
+            var allTicketsAfterDeletingNewTicket = ticketServiceTest.GetAllTickets();
+            CollectionAssert.DoesNotContain(allTicketsAfterAddingNewTicket, newTicket);
+        }
+    }
+}
--- UserTicketService/TicketService.cs
+++ UserTicketService/TicketService.cs
@@ -20,17 +20,26 @@ namespace UserTicketService
             return (ticket is null) ? throw new TicketNotFoundException() : ticket;
         }
 
-        private IEnumerable<Ticket> FakeBaseData
+        public void DeleteTicket(Ticket ticket)
         {
-            get
-            {
-                 return new List<Ticket>
-                 {
-                    new Ticket(1, "Москва - Санкт-Петербург", 3500),
-                    new Ticket(2, "Челябинск - Магадан", 3500),
-                    new Ticket(3, "Норильск - Уфа", 3500)
-                 };
-            }
+            FakeBaseData.Remove(ticket);
         }
+
+        public IEnumerable <Ticket> GetAllTickets()
+        {
+            return FakeBaseData;
+        }
+
+        public void SaveTicket(Ticket newTicket)
+        {
+            FakeBaseData.Add(newTicket);
+        }
+
+        private List<Ticket> FakeBaseData = new List<Ticket>
+        {
+            new Ticket(1, "Москва - Санкт-Петербург", 3500),
+            new Ticket(2, "Челябинск - Магадан", 3500),
+            new Ticket(3, "Норильск - Уфа", 3500)
+        };
     }
 }
